209df5328e6d72fe2ce3d1bdfbb09e1bcf69e1d5
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-15 15:32:12 +0000

    created some destination and hotels data

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
+import Home from '@/components/Home/Home';
 import React from 'react';
 
 const HomePage = () => {
-  return <div className="text-5xl">HomePage</div>;
+  return (
+    <div>
+     <Home />
+    </div>
+  );
 };
 
 export default HomePage;
-
-// localhost:3000
-// localhost:3000/tour
-// localhost:3000/tour/tour-details
-// localhost:3000/tour/tour-details/1
---
+++ data/data.ts
@@ -0,0 +1,89 @@
+export const destinationData = [
+  {
+    id: 1,
+    image: '/images/d1.jpg',
+    country: 'France',
+    travelers: '150,000',
+  },
+  {
+    id: 2,
+    image: '/images/d2.jpg',
+    country: 'USA',
+    travelers: '250,000',
+  },
+  {
+    id: 3,
+    image: '/images/d3.jpg',
+    country: 'Italy',
+    travelers: '180,000',
+  },
+  {
+    id: 4,
+    image: '/images/d4.jpg',
+    country: 'Japan',
+    travelers: '200,000',
+  },
+  {
+    id: 5,
+    image: '/images/d5.jpg',
+    country: 'Australia',
+    travelers: '120,000',
+  },
+  {
+    id: 6,
+    image: '/images/d6.jpg',
+    country: 'Canada',
+    travelers: '175,000',
+  },
+  {
+    id: 7,
+    image: '/images/d7.jpg',
+    country: 'Germany',
+    travelers: '160,000',
+  },
+  {
+    id: 8,
+    image: '/images/d8.jpg',
+    country: 'Brasil',
+    travelers: '140,000',
+  },
+];
+
+export const hotelsData = [
+  {
+    id: 1,
+    image: '/images/h1.jpg',
+    name: 'Best Western Swiss Cottage Hotel',
+    location: 'Westminster, London',
+    rating: '8.1',
+    reviews: '2.581',
+    price: '125',
+  },
+  {
+    id: 2,
+    image: '/images/h2.jpg',
+    name: 'Sonder Paseo de Gracia',
+    location: 'Barcelona, Spain',
+    rating: '8.4',
+    reviews: '2.639',
+    price: '248',
+  },
+  {
+    id: 3,
+    image: '/images/h3.jpg',
+    name: 'Park Hyatt New York ',
+    location: ' New York, United State',
+    rating: '8.4',
+    reviews: '2.639',
+    price: '248',
+  },
+  {
+    id: 4,
+    image: '/images/h4.jpg',
+    name: 'Hotel Quirinale',
+    location: 'Rome, Italy',
+    rating: '8.1',
+    reviews: '6.508',
+    price: '208',
+  },
+];
