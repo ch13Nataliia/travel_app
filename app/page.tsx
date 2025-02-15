5f0d9e0b6953d32c315e9081ad8d4901c3268c25
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-15 14:17:08 +0000

    initial files

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div className="text-5xl">HomePage</div>;
+};
+
+export default HomePage;
+
+// localhost:3000
+// localhost:3000/tour
+// localhost:3000/tour/tour-details
+// localhost:3000/tour/tour-details/1
