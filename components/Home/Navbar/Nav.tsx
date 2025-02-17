ec4fb142d8d19cff313d2f3ae301064032957dc7
Nataliia Tsirul <tcirulnatalya@gmail.com>
2025-02-17 10:11:02 +0000

    added scroll bg for navbar

--- components/Home/Navbar/Nav.tsx
+++ components/Home/Navbar/Nav.tsx
@@ -1,16 +1,32 @@
+'use client';
 import { navLinks } from '@/constant/constant';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaGlobe } from 'react-icons/fa';
 import { HiBars3BottomRight } from 'react-icons/hi2';
-
 
 type Props = {
   openNav: () => void;
 };
-const Nav = ({openNav}:Props) => {
+const Nav = ({ openNav }: Props) => {
+  const [navBg, setNavBg] = useState(false);
+
+  useEffect(() => {
+    const handler = () => {
+      if (window.scrollY >= 90) setNavBg(true);
+      if (window.scrollY < 90) setNavBg(false);
+    };
+
+    window.addEventListener('scroll', handler);
+    return () => window.removeEventListener('scroll', handler);
+  }, []);
+
   return (
-    <div className="bg-blue-950 transition-all duration-200 h-[12vh] z[1000] fixed w-full">
+    <div
+      className={` ${
+        navBg ? 'bg-blue-950 shadow-md' : 'fixed'
+      } transition-all duration-200 h-[12vh] z[1000] fixed w-full`}
+    >
       <div className="flex items-center h-full justify-between w-[90%] xl:w-[80%] mx-auto">
         {/* LOGO */}
         <div className="flex items-center space-x-2">
@@ -39,7 +55,10 @@ const Nav = ({openNav}:Props) => {
             Book Now
           </button>
           {/* BURGER MENU */}
-          <HiBars3BottomRight onClick={openNav} className="w-8 h-8 cursor-pointer text-white lg:hidden" />
+          <HiBars3BottomRight
+            onClick={openNav}
+            className="w-8 h-8 cursor-pointer text-white lg:hidden"
+          />
         </div>
       </div>
     </div>
